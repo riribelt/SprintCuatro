133336973b19b830951f1d18ede155510beb6bf0
ricardo-mt2022 <riribelt@yahoo.es>
2022-12-22 11:29:36 -0500

    Sprint 4 Ciclo 4 UNAB MisionTIC-2022

--- app/src/main/java/com/ricardo_rivera_beltran/sprintcuatro/Maps.java
+++ app/src/main/java/com/ricardo_rivera_beltran/sprintcuatro/Maps.java
@@ -13,6 +13,7 @@ public class Maps extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_maps);
+
     }
 }
